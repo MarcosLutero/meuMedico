a518bba6423d37f2fcedadead9dc8a543f934bef
Marcos Lutero <lutero_rocha@outlook.com>
2024-11-13 19:22:54 -0300

    Project created

---
+++ src/test/java/com/meuMedico/meumedico/MeumedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuMedico.meumedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeumedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
